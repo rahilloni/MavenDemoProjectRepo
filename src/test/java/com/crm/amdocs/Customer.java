20dc0d0b5d238367cc0ba965aee74bb7de5458cc
91935 <91935@DESKTOP-83QCFHP>
2022-01-01 22:50:50 +0530

    mavenDemoProject commit

---
+++ src/test/java/com/crm/amdocs/Customer.java
@@ -0,0 +1,18 @@
+package com.crm.amdocs;
+
+import org.testng.annotations.Test;
+
+public class Customer {
+	
+	@Test
+	public void createCustomer() {
+		
+		System.out.println("Customer created");
+		
+	}
+	
+	@Test
+	public void delteCustomer() {
+		System.out.println("Delete Customer");
+	}
+}
---
+++ src/test/java/com/crm/amdocs/Product.java
@@ -0,0 +1,17 @@
+package com.crm.amdocs;
+
+import org.testng.annotations.Test;
+
+public class Product {
+	
+	@Test
+	public void deleteProduct() {
+		System.out.println("product deleted");
+	}
+	
+	@Test
+	public void modifyProduct() {
+		System.out.println("Product modified");
+	}
+
+}
